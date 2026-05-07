2bd455b8070fdb91804d059c82b39cc32b1acd0b
Michael Workx <likhemayt@gmail.com>
2026-05-07 14:03:12 +0800

    M-1 check

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://innovate-proto.vercel.app',
+  site: 'https://innovate-proto.vercel.app', // <--- This is the M-1 fix
 
   vite: {
     plugins: [tailwindcss()]
